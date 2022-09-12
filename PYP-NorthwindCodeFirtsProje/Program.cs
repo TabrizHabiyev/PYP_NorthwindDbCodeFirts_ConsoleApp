1c7484af3c592f75e742f1ffa9c4683a08d6d89c
Tabriz Habiyev <tabrizmonzo@gmail.com>
2022-09-12 04:02:38 +0400

    Add project files.

---
+++ PYP-NorthwindCodeFirtsProje/Program.cs
@@ -0,0 +1,16 @@
+﻿using PYP_NorthwindCodeFirts.Models;
+using PYP_NorthwindCodeFirtsProje.Models;
+
+NORTHWNDContext context = new();
+
+context.Categories.Add(new Category { CategoryName = "Example", Description = "Example" });
+
+
+context.Categories.Update(new Category { CategoryId = 294, CategoryName = "Example", Description= "Example" });
+
+
+Category category=context.Categories.FirstOrDefault(x => x.CategoryId == 294);
+
+
+context.Categories.Remove(category);
+context.SaveChanges();
